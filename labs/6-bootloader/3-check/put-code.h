666212c37c0543df06522579792169e980459976
engler <ddd.rrr.eee@gmail.com>
2025-01-28 16:27:49 -0800

    minor

--- labs/6-bootloader/3-check/put-code.h
+++
@@ -1,6 +0,0 @@
-#ifndef __PUT_CODE_H__
-#define __PUT_CODE_H__
-
-// nothing.
-
-#endif
--- labs/6-bootloader/3-check/sim.h
+++
@@ -1,5 +0,0 @@
-#include "rpi-thread.h"
-#include "src-loc.h"
-
-void pi_client(void *ctx);
-void unix_server(void *ctx);
